657ae1ae90ebb5991b29075e1fbc0de44208e17a
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-17 11:58:08 -0300

    logica com javaScript

--- atividades/ex03f/ex03f.js
+++ atividades/ex03f/ex03f.js
@@ -1,6 +1,12 @@
 let pessoa = prompt("Tem internet");
-let jogar = prompt("Atende os requisitos");
+let controle = prompt("Você tem controle do console?");
+let tempo = prompt("Você tem tempo disponível?");
+let estudo = prompt("Você está em horário de aula?");
 
 verifica1 = pessoa =="sim";
-verifica2 = jagar == "sim";
-alert("")
+verifica2 = controle == "sim";
+verifica3 = tempo == "sim";
+verifica4 = estudo == "sim";
+
+if (true(alert("Você pode jogar!")));
+else alert("Você não pode jogar!");
