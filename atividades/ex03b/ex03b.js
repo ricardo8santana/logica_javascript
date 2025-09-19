39c76660b926ffb6b9aeb39f15db51523ef05dce
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-19 10:55:39 -0300

    logica com javaScript

--- atividades/ex03b/ex03b.js
+++ atividades/ex03b/ex03b.js
@@ -1,5 +1,5 @@
 let clienteVip = prompt("Você é Client vip");
-let desconto = parseFloat(prompt("Você gastor mais de 150 r"));
-verifica1 = clienteVip == "sim" || desconto > 150;
+let compra = parseFloat(prompt("Digite o valor da compra"));
+verifica1 = clienteVip == "sim" || compra > 150;
 
 alert("Você terá um desconto" + verifica1 );
