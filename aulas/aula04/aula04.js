e33e2293b75ce96d8ad99f2f3dfaf39b66224b71
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-22 11:55:59 -0300

    logica com javaScript

--- aulas/aula04/aula04.js
+++ aulas/aula04/aula04.js
@@ -14,3 +14,14 @@ if(idade >= 16){
 }else {
     alert("Não pode votar");
 }
+
+/*
+1.Faça um fluxograma que receba a idade de uma pessoa e verifique se ela é maior de idade 
+(18 anos ou mais) ou  menor de idade.
+
+2.Faça um fluxograma que  receba um número e verifique se ele é positivo ou negativo.
+3.Faça um fluxograma que leia a nota de aluno  e verifique se ele está aprovado (nota maior ou igual a 6)
+ou reprovado (nota menor que 6).
+4.Elabore um fluxograma que receba a senha digitada pelo usuário e verifique se ela é 1234.
+Caso seja, mostre "Acesso Permitido". Caso contrario, mostre "Acesso Negado".
+*/
