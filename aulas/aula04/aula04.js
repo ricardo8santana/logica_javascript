13554ef9a9f26b4e9f0a2ef8cbacaadc42d22398
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-22 11:28:32 -0300

    logica com javaScript

---
+++ aulas/aula04/aula04.js
@@ -0,0 +1,16 @@
+/*
+Condominios: Criam caminhos no código que permitem criar seleções de bloco específicos de códigos.
+Para esse tipo de bikfurcação utilizamos os comandos:
+
+IF (se) --> Para este comando ser executado, precisamos que a operação relacional e/ou lógica seja TRUE
+
+ELSE (SENÃO) --> Para este comando ser executado, precisamos que a operação do IF seja falsa.
+
+*/
+
+let idade = 18;
+if(idade >= 16){
+    alert("alert votar");
+}else {
+    alert("Não pode votar");
+}
