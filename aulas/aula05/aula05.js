70628bd63ba37d4aca2866aa659cb49b1a1c220f
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-24 11:23:11 -0300

    lógica com javaScript

---
+++ aulas/aula05/aula05.js
@@ -0,0 +1,9 @@
+let numero = parseFloat(prompt("Digite um número:"));
+
+if(numero > 0){
+    alert("Numero positivo");
+}else if(numero < 0){
+    alert("Numero negativo!")
+}else{
+    alert("Neutro");
+}
