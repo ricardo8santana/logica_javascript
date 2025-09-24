da09c39bc3366e99889bec9a01ad9cec6d601360
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-24 11:54:42 -0300

    lógica com javaScript

---
+++ atividades/ex05c/ex05.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("Fale um número"));
+
+if (numero == 0){
+    alert("Número neutro");
+}else if(numero%2 == 0){
+    alert("Número par");
+}else{
+    alert("Número impar");
+}
