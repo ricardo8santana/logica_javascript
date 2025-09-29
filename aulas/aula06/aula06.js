12918e929fe992b298c79f6129f018fed99ba01b
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-09-29 08:36:31 -0300

    lógica javaScript

---
+++ aulas/aula06/aula06.js
@@ -0,0 +1,12 @@
+/*
+O Switch/Case cria uma condicional que busca valores de igualdades para cada um dos valores buscados
+Sitaxe:
+
+switch (variavel){
+    case "valor1":
+        comandos
+        case "valor2"
+        comandos
+}
+
+*/
