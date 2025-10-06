34c6444f9479b5483481eef1b3f4c0ec6747383c
LUIS RICARDO DE SANTANA <luis.rsantana@senacsp.edu.br>
2025-10-06 11:35:18 -0300

    laços de repetição while

---
+++ atividades/ex07/ex07.js
@@ -0,0 +1,14 @@
+let i = 1
+let numero = parseInt(prompt("Digite um numero de 1 a 10"));
+
+while(i <= 10){
+     console.log( "tabuada" + numero + "x" + i +  "="  + numero * i );
+    i = i +1
+}
+
+// let contador = 1;
+
+// while(contador <=10){
+//     console.log(contador);
+//     contador = contador + 1;
+// }
--- aulas/aula07/aula07.js
+++ aulas/aula07/aula07.js
@@ -1,6 +1,13 @@
-let numero = parseInt(prompt("Digite um numero:"));
+// let numero = parseInt(prompt("Digite um numero:"));
 
-while(numero <= 10){
-    console.log(numero);
-    numero = numero + 1;
+// while(numero >= 10){
+//     console.log(numero);
+//     numero = numero - 1;
+// }
+
+let palavraSecreta = prompt("Digite a palavra secreta");
+
+while(palavraSecreta != 'abracadabra'){
+    palavraSecreta = prompt("Você errou! Tente novamente");
 }
+alert("Acertou miseravi");
